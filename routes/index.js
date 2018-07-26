df43b3ef44b92de26aacab331e522e18ce06d4cd
Shams <shabbar0333@gmail.com>
2018-07-26 14:53:37 -0700

    removed apiRouter variable

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
-const apiRouter = require('./api');
 
 router.get('/', (req, res) => res.send('welcome to mongo api'));
-router.use('/api', apiRouter);
+router.use('/api', require('./auth.route'));
 router.use('/auth', require('./auth.route'));
 
 module.exports = router;
