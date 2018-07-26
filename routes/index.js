9067dfa6434ee86ef5bf1640b54cc1edebe4396b
Shams <shabbar0333@gmail.com>
2018-07-26 15:03:32 -0700

    fixed api require

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => res.send('welcome to mongo api'));
-router.use('/api', require('./auth.route'));
+router.use('/api', require('./api'));
 router.use('/auth', require('./auth.route'));
 
 module.exports = router;
