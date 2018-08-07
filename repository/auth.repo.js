7403056839fe6eb5175417290047e4234e9c2eee
Shams <shabbar0333@gmail.com>
2018-08-07 03:40:17 -0700

    implemented signup and login

--- repository/auth.repo.js
+++ repository/auth.repo.js
@@ -1,20 +1,36 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const config = require('../utils/config');
 
-const signup = async ({firstname, lastname, email, password}) => {
-    const existingUser = await User.findOne({ where: { email }});
+const signup = async (user) => {
+    console.log('user :', user);
+    const existingUser = await User.findOne({ email: user.email });
     if(existingUser)
         throw new Error("a user already exist with this email");
     else {
-        let user = new User({firstname, lastname, email, password});
-        let token = jwt.sign({user: "user"}, config.JWT_SECRET);
-        return token;
+        user.password = bcrypt.hashSync(user.password, 12);
+        let newUser = new User(user);
+        let result = await newUser.save();
+        console.log('result :', result);
+        let token = jwt.sign({user: result.id}, config.JWT_SECRET);
+        return { user: result.id, token: token };
     }
 };
 
 const login = async ({ email, password }) => {
-    
+    const user = await User.findOne({ email }).select("+password");
+    if(user){
+        let success = bcrypt.compareSync(password, user.password);
+        if(success){
+            let token = jwt.sign({user: user.id}, config.JWT_SECRET);
+            return { user: user.firstname, token: token };
+        }
+        else
+            throw new Error("wrong password");
+    }else {
+        throw new Error("user with this email doesnot exist");
+    }
 };
 
 module.exports = { signup, login };
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,14 +1,15 @@
 const authRepo = require('../repository/auth.repo');
+const wrap = require('../utils/asyncWrapper');
 const router = require('express').Router();
 
-router.post('/signup', async(req, res) => {
-    let result = await authRepo.signup({});
-    res.send('signup '+result);
-});
+router.post('/signup', wrap(async(req, res) => {
+    let result = await authRepo.signup(req.body);
+    res.json(result);
+}));
 
-router.post('/login', async(req, res) => {
-    let result = await authRepo.login({});
-    res.send('login');
-});
+router.post('/login', wrap(async(req, res) => {
+    let result = await authRepo.login(req.body);
+    res.json(result);
+}));
 
 module.exports = router;
